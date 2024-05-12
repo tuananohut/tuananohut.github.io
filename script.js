75d0ede3123910d11b4c07ed7d1b39e47d5dbe1b
Tuana <57767763+tuananohut@users.noreply.github.com>
2024-05-12 19:25:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var c = document.getElementById("introduction");
+var ctx = c.getContext("2d");
+ctx.font = "25px Sans-Serif";
+ctx.fillStyle = "#ffffff";
+ctx.fillText("I want to write whole text here.", 50, 50);
