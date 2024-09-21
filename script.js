bada0f28d5d353639e8383ed14300a65c8133ffc
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-21 14:02:18 +0300

    other projects added

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ function closeModal(modalId) {
   modal.style.display = "none"; // Hide the modal
 }
 
-// Close the modal when clicking outside of it
 window.onclick = function (event) {
   if (event.target.classList.contains('modal')) {
     event.target.style.display = "none";
   }
 };
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n >= slides.length) {slideIndex = 0} 
+  if (n < 0) {slideIndex = slides.length - 1} 
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex].style.display = "block";  
+}
